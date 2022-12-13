21fed09b9e9b2b4a631005dd522c92a1296e4fcb
bui bao long <blong1102@gmail.com>
2022-12-13 12:37:45 +0700

    feat: final answer

--- min-max-sum.js
+++ min-max-sum.js
@@ -1,2 +1,10 @@
-const minMaxSum = (array) => {}
-exports.miniMaxSum = minMaxSum
+const minMaxSum = (array) => {
+  const totalSum = array.reduce((result, current) => result + current)
+
+  //max sum of 4 elements will be the total sum of array minus the smallest element
+  const max = totalSum - Math.min(...array)
+  //min sum of 4 elements will be the total sum of array minus the largest element
+  const min = totalSum - Math.max(...array)
+  return { min, max }
+}
+exports.minMaxSum = minMaxSum
