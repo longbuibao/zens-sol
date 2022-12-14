23ed36389f4964269d009ebde77b7cf81b458285
bui bao long <blong1102@gmail.com>
2022-12-14 23:29:09 +0700

    fix: change to request to localhost

--- jokee/front-end/src/api/aixos.js
+++ jokee/front-end/src/api/aixos.js
@@ -1,13 +1,7 @@
 import axios from "axios";
 
-console.log(
-  import.meta.env.PROD ? "http://back_end:3000" : "http://localhost:3000"
-);
-
 const axiosClient = axios.create({
-  baseURL: import.meta.env.PROD
-    ? "http://back_end:3000"
-    : "http://localhost:3000",
+  baseURL: "http://localhost:5000",
 });
 
 export default axiosClient;
