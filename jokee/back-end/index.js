996eebf0b317098b1570504187de1ffc6361a2a1
bui bao long <blong1102@gmail.com>
2022-12-14 10:40:06 +0700

    feat: add cors()

--- jokee/back-end/index.js
+++ jokee/back-end/index.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 const jokeRouter = require("./routes/joke.route");
 
 const app = express();
 
-app.use(cors());
+app.use(cookieParser());
+app.use((req, res, next) => {
+  res.header("Content-Type", "application/json;charset=UTF-8");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+
 app.use(jokeRouter);
 
 app.listen(3000);
