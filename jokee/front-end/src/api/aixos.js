749bc902eb3d256134e50f5ac15631f631b67b3e
bui bao long <blong1102@gmail.com>
2022-12-14 16:08:47 +0700

    feat: testing docker-compose

--- jokee/front-end/src/api/aixos.js
+++ jokee/front-end/src/api/aixos.js
@@ -1,7 +1,13 @@
 import axios from "axios";
 
+console.log(
+  import.meta.env.PROD ? "http://back_end:3000" : "http://localhost:3000"
+);
+
 const axiosClient = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: import.meta.env.PROD
+    ? "http://back_end:3000"
+    : "http://localhost:3000",
 });
 
 export default axiosClient;
