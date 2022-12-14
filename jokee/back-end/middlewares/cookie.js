9ea795f4f54eb4e16ed93b8f8c3bf8b05ed8de98
bui bao long <blong1102@gmail.com>
2022-12-14 10:54:43 +0700

    fix: remove user and like table

--- jokee/back-end/middlewares/cookie.js
+++ jokee/back-end/middlewares/cookie.js
@@ -1,25 +1,6 @@
-const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
-const crypto = require("crypto");
-
 const checkCookie = async (req, res, next) => {
   try {
-    const userIdFromCookie = req.cookies.userId || "";
-    const jokeIdsFromCookie = req.cookies.jokeIds || [];
-
-    if (!userIdFromCookie) {
-      const userId = crypto.randomBytes(16).toString("hex");
-      await prisma.user.create({
-        data: {
-          id: userId,
-        },
-      });
-      req.userId = userId;
-      req.jokeIds = [];
-      return next();
-    }
-
-    req.userId = userIdFromCookie;
+    const jokeIdsFromCookie = req.cookies.jokeIds || "[]";
     req.jokeIds = JSON.parse(jokeIdsFromCookie);
     next();
   } catch (error) {
